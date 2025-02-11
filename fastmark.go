d259613685336d4a62511238fe2f006f274ee97a
Andre Renaud <andre@ignavus.net>
2025-02-12 09:49:14 +1300

    Initial commit
    Enough to quickly look through darknet training data

---
+++ fastmark.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/sqweek/dialog"
+
+	"github.com/AllenDang/giu"
+)
+
+var (
+	files      []string
+	fileRows   []*giu.TableRowWidget
+	fileLabels []*giu.SelectableWidget
+
+	currentImage   *giu.Texture
+	selectedIndex  int
+	currentRegions []Region
+
+	directory string
+	splitPos  = float32(200)
+)
+
+type Region struct {
+	xMid   float64
+	yMid   float64
+	width  float64
+	height float64
+
+	index int
+}
+
+func parseLabelData(filename string) []Region {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Printf("Error opening file %s: %s", filename, err)
+		return nil
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var retval []Region
+	for scanner.Scan() {
+		columns := strings.Fields(scanner.Text())
+		if len(columns) != 5 {
+			log.Printf("Invalid line: %s", scanner.Text())
+			continue
+		}
+		region := Region{}
+		region.index, err = strconv.Atoi(columns[0])
+		region.xMid, err = strconv.ParseFloat(columns[1], 64)
+		region.yMid, err = strconv.ParseFloat(columns[2], 64)
+		region.width, err = strconv.ParseFloat(columns[3], 64)
+		region.height, err = strconv.ParseFloat(columns[4], 64)
+		retval = append(retval, region)
+	}
+
+	return retval
+}
+
+func (r Region) Color() color.RGBA {
+	switch r.index {
+	case 1:
+		return color.RGBA{255, 0, 0, 255}
+	case 2:
+		return color.RGBA{0, 255, 0, 255}
+	case 3:
+		return color.RGBA{0, 0, 255, 255}
+	case 4:
+		return color.RGBA{255, 255, 0, 255}
+	case 5:
+		return color.RGBA{255, 0, 255, 255}
+	case 6:
+		return color.RGBA{0, 255, 255, 255}
+	default:
+		return color.RGBA{128, 128, 128, 255}
+	}
+}
+
+func drawFile(filename string) {
+	currentImage = nil
+	fullFilename := filepath.Join(directory, filename)
+	if rgba, err := giu.LoadImage(fullFilename); err != nil {
+		log.Printf("Error loading image %s: %s", filename, err)
+	} else {
+		giu.EnqueueNewTextureFromRgba(rgba, func(t *giu.Texture) {
+			currentImage = t
+		})
+	}
+
+	ext := filepath.Ext(filename)
+
+	labelFile := filepath.Join(directory, "../labels/", strings.TrimSuffix(filename, ext)+".txt")
+
+	currentRegions = parseLabelData(labelFile)
+}
+
+func selectDirectory() {
+	newDirectory, err := dialog.Directory().Title("Load images").Browse()
+	if err != nil {
+		log.Printf("Error selecting directory: %s", err)
+		return
+	}
+	directory = newDirectory
+	updateFiles()
+
+}
+
+func selectFile(i int) {
+	if i < 0 || i >= len(files) {
+		log.Printf("Invalid file index %d", i)
+		return
+	}
+	if selectedIndex > 0 && selectedIndex < len(fileLabels) {
+		old := fileLabels[selectedIndex]
+		if old != nil {
+			old.Selected(false)
+		}
+	}
+	file := files[i]
+	selectedIndex = i
+	fileLabels[i].Selected(true)
+	drawFile(file)
+}
+
+func updateFiles() {
+	files = []string{}
+
+	match, err := filepath.Glob(filepath.Join(directory, "*"))
+	if err != nil {
+		log.Printf("Error listing files in %s: %s", directory, err)
+	} else {
+		for _, m := range match {
+			if strings.HasSuffix(m, ".png") || strings.HasSuffix(m, ".jpg") {
+				files = append(files, filepath.Base(m))
+			}
+		}
+	}
+	fileRows = make([]*giu.TableRowWidget, len(files))
+	fileLabels = make([]*giu.SelectableWidget, len(files))
+	for i, file := range files {
+		fileLabels[i] = giu.Selectable(file)
+		fileLabels[i].OnClick(func() {
+			selectFile(i)
+		})
+		fileRows[i] = giu.TableRow(fileLabels[i])
+	}
+	selectFile(0)
+}
+
+func loop() {
+	window := giu.SingleWindow()
+	var file string
+	if selectedIndex >= 0 && selectedIndex < len(files) {
+		file = files[selectedIndex]
+	}
+
+	window.Layout(
+		giu.Label(fmt.Sprintf("Fast Mark image tagging %d/%d images", selectedIndex, len(files))),
+		giu.SplitLayout(giu.DirectionVertical, &splitPos,
+			giu.Table().
+				FastMode(true).
+				Columns(giu.TableColumn("Files")).
+				Rows(fileRows...).
+				Size(giu.Auto, giu.Auto),
+			giu.Column(
+				giu.Row(
+					giu.Button("Change Directory").OnClick(selectDirectory),
+					giu.Label(directory),
+				),
+				giu.Label(file),
+				giu.Custom(func() {
+					canvas := giu.GetCanvas()
+					pos := giu.GetCursorScreenPos()
+					imageWidth := 800
+					imageHeight := 600
+
+					if currentImage != nil {
+						max := pos.Add(image.Point{X: imageWidth, Y: imageHeight})
+						canvas.AddImage(currentImage, pos, max)
+					}
+					for _, region := range currentRegions {
+						w := int(float32(region.width) * float32(imageWidth))
+						h := int(float32(region.height) * float32(imageHeight))
+						x := int(float32(region.xMid)*float32(imageWidth)) - w/2
+						y := int(float32(region.yMid)*float32(imageHeight)) - h/2
+						color := region.Color()
+						canvas.AddRect(pos.Add(image.Point{X: x, Y: y}), pos.Add(image.Point{X: x + w, Y: y + h}), color, 0, 0, 2)
+					}
+				}),
+			),
+		),
+	)
+
+	if giu.IsKeyPressed(giu.KeyDown) || giu.IsKeyPressed(giu.KeyJ) {
+		selectFile(selectedIndex + 1)
+	}
+	if giu.IsKeyPressed(giu.KeyUp) {
+		selectFile(selectedIndex - 1)
+	}
+}
+
+func main() {
+	wnd := giu.NewMasterWindow("Fast Mark Image Tagging", 1024, 768, 0)
+
+	updateFiles()
+	wnd.Run(loop)
+}
